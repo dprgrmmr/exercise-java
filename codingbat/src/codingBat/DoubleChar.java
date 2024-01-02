4e816c35dd3e32c706ebeb8561a5f791e682dafc
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-01 19:02:55 -0500

    update doubleChar

--- codingbat/src/codingBat/DoubleChar.java
+++ codingbat/src/codingBat/DoubleChar.java
@@ -1,5 +1,12 @@
 package codingBat;
 
 public class DoubleChar {
-
+	
+	public String doubleChar(String str) {
+		  String word = "";
+		  for (int j = 0; j < str.length(); j++){
+		  word += str.substring(j, j+1) + str.substring(j, j+1);
+		  }
+		  return word;
+		}
 }
