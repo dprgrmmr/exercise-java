a210a8e0e5a2d2238cd3b5bc4cf24565eb7f5503
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-05 22:16:30 -0500

    Passed rotateLeft test case

--- codingbat/src/codingBat/RotateLeft.java
+++ codingbat/src/codingBat/RotateLeft.java
@@ -1,17 +1,22 @@
 package codingBat;
 
+import java.util.Arrays;
+
 public class RotateLeft {
 
 	public int[] rotateLeft(int[] arr) {
-		
-		
-		return null;
+		int temp = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			arr[i - 1] = arr[i];
+		}
+		arr[arr.length - 1] = temp;
+		return arr;
 	}
 	
 	public static void main(String[] args) {
 		RotateLeft rotateLeft = new RotateLeft();
 		int[] arr = new int[] {1, 2, 3, 4}; //use initializer to initiate a four element array
 		int[] result = rotateLeft.rotateLeft(arr);
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
