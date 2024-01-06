1b48d8c2c36e4044bd1215086f1f5a973e5d6e4c
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-05 22:26:59 -0500

    Check for special cases

--- codingbat/src/codingBat/RotateLeft.java
+++ codingbat/src/codingBat/RotateLeft.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 public class RotateLeft {
 
 	public int[] rotateLeft(int[] arr) {
+		if (arr == null) {
+			return null;
+		}
+		
+		if (arr.length == 0) {
+			return arr;
+		}
+		
 		int temp = arr[0];
 		for (int i = 1; i < arr.length; i++) {
 			arr[i - 1] = arr[i];
@@ -17,6 +25,12 @@ public class RotateLeft {
 		RotateLeft rotateLeft = new RotateLeft();
 		int[] arr = new int[] {1, 2, 3, 4}; //use initializer to initiate a four element array
 		int[] result = rotateLeft.rotateLeft(arr);
+		System.out.println(Arrays.toString(result));
+		
+		result = rotateLeft.rotateLeft(null);
+		System.out.println(Arrays.toString(result));
+		
+		result = rotateLeft.rotateLeft(new int[] {});
 		System.out.println(Arrays.toString(result));
 	}
 }
