b3ddf65b8335b89dba3c8965a2829fb29fb5d91e
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-05 21:43:26 -0500

    initial implementation of rotateLeft

---
+++ codingbat/src/codingBat/RotateLeft.java
@@ -0,0 +1,17 @@
+package codingBat;
+
+public class RotateLeft {
+
+	public int[] rotateLeft(int[] arr) {
+		
+		
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		RotateLeft rotateLeft = new RotateLeft();
+		int[] arr = new int[] {1, 2, 3, 4}; //use initializer to initiate a four element array
+		int[] result = rotateLeft.rotateLeft(arr);
+		System.out.println(result);
+	}
+}
