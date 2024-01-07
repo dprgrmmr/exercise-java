ebe7c0724b06870f9bfc08b6a593ce1052f82ee8
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-07 18:15:36 -0500

    Checked for special cases

--- codingbat/src/codingBat/RotateLeftK.java
+++ codingbat/src/codingBat/RotateLeftK.java
@@ -6,6 +6,14 @@ public class RotateLeftK {
 
 	public int[] rotateLeftK(int[] arr, int k) {
 		
+		if (arr == null) {
+			return null;
+		}
+		
+		if (arr.length == 0) {
+			return arr;
+		}
+		
 		for (int j = 0; j < k; j++) {
 		int temp = arr[0];
 		for (int i = 1; i < arr.length; i++) {
@@ -20,6 +28,11 @@ public class RotateLeftK {
 		RotateLeftK rtLeft = new RotateLeftK();
 		int[] arr = new int[] {1, 3, 5, 8, 7};
 		int[] result = rtLeft.rotateLeftK(arr, 2);
+		System.out.println(Arrays.toString(result));
+		
+		result = rtLeft.rotateLeftK(null, 2);
+		System.out.println(Arrays.toString(result));
+		result = rtLeft.rotateLeftK(new int[] {}, 1);
 		System.out.println(Arrays.toString(result));
 	}
 }
