8ffcf411c60217dae7e1d18da388726a8c66fa35
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-07 18:13:04 -0500

    Check-in of functioning rotateLeftK method

--- codingbat/src/codingBat/RotateLeftK.java
+++ codingbat/src/codingBat/RotateLeftK.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 public class RotateLeftK {
 
 	public int[] rotateLeftK(int[] arr, int k) {
-		return null;
+		
+		for (int j = 0; j < k; j++) {
+		int temp = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			arr[i - 1] = arr[i];
+		}
+		arr[arr.length - 1] = temp;
+		}
+		return arr;
 	}
-	
-	
 	
 	public static void main(String[] args) {
 		RotateLeftK rtLeft = new RotateLeftK();
 		int[] arr = new int[] {1, 3, 5, 8, 7};
 		int[] result = rtLeft.rotateLeftK(arr, 2);
-		System.out.println(Arrays.toString(result));
-		
-		result = rtLeft.rotateLeftK(null, 2);
-		System.out.println(Arrays.toString(result));
-		
-		result = rtLeft.rotateLeftK(new int[] {}, 2);
 		System.out.println(Arrays.toString(result));
 	}
 }
