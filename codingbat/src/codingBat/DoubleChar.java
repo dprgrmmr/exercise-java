ab008f8c9c169aa7bc3e157b44ddbdd02e39a1d8
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-06 23:02:42 -0500

    Checked for special cases

--- codingbat/src/codingBat/DoubleChar.java
+++ codingbat/src/codingBat/DoubleChar.java
@@ -6,13 +6,26 @@ public class DoubleChar {
 		DoubleChar doubleChar = new DoubleChar();
 		String result = doubleChar.doubleChar("abcd");
 		System.out.println("The result equals " + result);
+		System.out.println("doubleChar of null is " + doubleChar.doubleChar(null));
+		System.out.println("DoubleChar of \"\" is " + doubleChar.doubleChar(""));
 	}
+	
 	public String doubleChar(String str) {
 		  StringBuffer result = new StringBuffer();
-		  for (char c : str.toCharArray()){
-		  result.append(c);
-		  result.append(c);
+		  
+		  if (str == null) {
+			  return null;
 		  }
+		  
+		  if (str.isEmpty()) {
+			  return str;
+		  }
+		  
+		  for (char c : str.toCharArray()){
+			  result.append(c);
+			  result.append(c);
+		  }
+		  
 		  return result.toString();
 		}
 }
