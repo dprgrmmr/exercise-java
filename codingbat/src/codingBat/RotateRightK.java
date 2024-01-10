55fd9120cc20d2899bb78420d17c9f100a322129
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-09 21:22:54 -0500

    Completed rotateRightK method

--- codingbat/src/codingBat/RotateRightK.java
+++ codingbat/src/codingBat/RotateRightK.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 public class RotateRightK {
 
 	public int[] rotateRightK(int[] arr, int k) {
-		return null;
+		for (int j = 0; j < k; j++) {
+			int temp = arr[arr.length - 1];
+			for (int i = arr.length - 1; i > 0; i--) {
+				arr[i] = arr[i - 1];
+			}
+			arr[0] = temp;
+			}
+			return arr;
 	}
 	
 	public static void main(String[] args) {
