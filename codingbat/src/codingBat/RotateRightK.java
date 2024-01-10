44488130af6958bc0e791f5446ceecc6340b2d27
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-09 21:19:36 -0500

    Initial check-in of rotateRightK method

---
+++ codingbat/src/codingBat/RotateRightK.java
@@ -0,0 +1,20 @@
+package codingBat;
+
+import java.util.Arrays;
+
+public class RotateRightK {
+
+	public int[] rotateRightK(int[] arr, int k) {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		RotateRightK rtRight = new RotateRightK();
+		int[] arr = new int[] {6, 7, 9, 0};
+		int[] result = rtRight.rotateRightK(arr, 3);
+		System.out.println(Arrays.toString(result));
+	
+	
+	
+	}
+}
