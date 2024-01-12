d226e32f2615df4cd78e9351f2d08e621040a32f
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-11 21:56:19 -0500

    Created 3 test functions

--- codingbat/src/codingBat/RotateRightTest.java
+++ codingbat/src/codingBat/RotateRightTest.java
@@ -17,15 +17,24 @@ public class RotateRightTest {
 		for (int i = 0; i < expected.length; i++) {
 			assertEquals(expected[i], result[i]);
 		}
-		
-	
-		result = rtRight.rotateRight(null);
-		assertEquals(null, result);
-		
+	}
+
+	@Test
+	final void rotateRightOfEmptyArray() {
+		RotateRight rtRight = new RotateRight();
+		int[] arr = new int[] {1, 2, 3, 4}; 
+		int[] result = rtRight.rotateRight(new int[] {});
 		int[] expected2 = new int[0];
-		result = rtRight.rotateRight(new int[] {});
 		for (int i = 0; i < expected2.length; i++) {
 			assertEquals(expected2[i], result[i]);
 		}
+	}
+	
+	@Test
+	final void rotateRightOfNull() {
+		RotateRight rtRight = new RotateRight();
+		int[] arr = new int[] {1, 2, 3, 4}; 
+		int[] result = rtRight.rotateRight(null);
+		assertEquals(null, result);
 	}
 }
