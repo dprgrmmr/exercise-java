561bb9af88f598428841193106dc0a0432981629
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-16 10:41:22 -0500

    Completed rotateK method

--- codingbat/src/codingBat/RotateK.java
+++ codingbat/src/codingBat/RotateK.java
@@ -5,7 +5,24 @@ import java.util.Arrays;
 public class RotateK {
 
 	public int[] rotateK(int[] arr, int k) {
-		return null;
+		RotateRightK rtRightPos = new RotateRightK();
+		RotateLeftK rtLeftNeg = new RotateLeftK();
+		if (arr == null) {
+			return null;
+		}
+		
+		if (arr.length == 0) {
+			return arr;
+		}
+		
+		if (k == 0) {
+			return arr;
+		}
+		
+		if (k < 0) {
+			return rtLeftNeg.rotateLeftK(arr, (0 - k));
+		}
+		return rtRightPos.rotateRightK(arr, k);
 	}
 	
 	public static void main(String[] args) {
@@ -17,7 +34,7 @@ public class RotateK {
 		result = rotatek.rotateK(null, 1);
 		System.out.println(Arrays.toString(result));
 		
-		result = rotatek.rotateK(null, -1);
+		result = rotatek.rotateK(arr, -1);
 		System.out.println(Arrays.toString(result));
 		
 		result = rotatek.rotateK(new int[] {}, 1);
