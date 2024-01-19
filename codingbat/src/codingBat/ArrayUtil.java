72fde02bf6c92a9a2b220a627736dee71095c311
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-18 21:21:07 -0500

    Checked for special cases in reverse method

--- codingbat/src/codingBat/ArrayUtil.java
+++ codingbat/src/codingBat/ArrayUtil.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 public class ArrayUtil {
 
 	public static int[] reverse(int[] arr) {
+
+		if (arr == null) {
+			return null;
+		}
+		
+		if (arr.length == 0) {
+			return arr;
+		}
+		
 		int temp = 0;
 		for (int i = 0; i < arr.length / 2; i++) {
 			temp = arr[i];
@@ -18,6 +27,12 @@ public class ArrayUtil {
 		ArrayUtil reverse = new ArrayUtil();
 		int[] arr = new int[] {1, 2, 3, 4, 5};
 		int[] result = reverse.reverse(arr);
+		System.out.println(Arrays.toString(result));
+		
+		result = reverse.reverse(null);
+		System.out.println(Arrays.toString(result));
+		
+		result = reverse.reverse(new int[0]);
 		System.out.println(Arrays.toString(result));
 	}
 }
