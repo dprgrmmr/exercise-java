4287d734bd7625d867b5fd50b9c50dd50e0b0bac
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-24 22:18:23 -0500

    Completed max() method finds max integer in an int array

--- codingbat/src/codingBat/MaxNum.java
+++ codingbat/src/codingBat/MaxNum.java
@@ -3,12 +3,19 @@ package codingBat;
 public class MaxNum {
 
 	public static int max(int[] array) {
-		return 0;
+		int max = Integer.MIN_VALUE;
+		for (int i : array) {
+			if (i > max) {
+				max = i;
+			}
+		}
+		return max;
 	}
 	
 	public static void main(String[] args) {
 		MaxNum maxInt = new MaxNum();
-		int[] arr = new int[] {1, 3, 10, 8};
+		int[] arr = new int[] {1, 82, 10, 24, 3};
 		int result = maxInt.max(arr);
+		System.out.println(result);
 	}
 }
