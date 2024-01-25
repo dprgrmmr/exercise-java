da571863fbaddeb0ac8cc592851f3a85c811dd96
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-24 22:12:26 -0500

    Initial check-in of MaxNum class

---
+++ codingbat/src/codingBat/MaxNum.java
@@ -0,0 +1,14 @@
+package codingBat;
+
+public class MaxNum {
+
+	public static int max(int[] array) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		MaxNum maxInt = new MaxNum();
+		int[] arr = new int[] {1, 3, 10, 8};
+		int result = maxInt.max(arr);
+	}
+}
