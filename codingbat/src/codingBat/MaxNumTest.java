23349ad6b38001dc4d6a731291ee30cbeb9aeaa7
dprgrmr <vinegamerzthechannel@gmail.com>
2024-01-26 22:15:53 -0500

    Updated to test for special case

--- codingbat/src/codingBat/MaxNumTest.java
+++ codingbat/src/codingBat/MaxNumTest.java
@@ -12,4 +12,12 @@ public class MaxNumTest {
 				ArrayUtil.max(
 						new int[] {1, 55, 24, 82, 67}));
 	}
+	
+	@Test 
+	public void maxNumEmptyArray() {
+		Assertions.assertEquals(0, 
+				ArrayUtil.max(
+						new int[0]));
+		
+	}
 }
