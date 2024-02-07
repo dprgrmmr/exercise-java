8d96e58998b9746f1c335bb8fa2302b03377eb62
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-06 21:23:39 -0500

    Completed hasDoubleK() method to return true if there are at least two consecutive appearances of k in the array

--- codingbat/src/codingBat/ArrayUtil.java
+++ codingbat/src/codingBat/ArrayUtil.java
@@ -208,6 +208,11 @@ public class ArrayUtil {
 	}
 	
 	public static boolean hasDoubleK(int[] arr, int k) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] == 2 && arr[i + 1] == 2) {
+				return true;
+			}
+		}
 		return false;
 	}
 }
