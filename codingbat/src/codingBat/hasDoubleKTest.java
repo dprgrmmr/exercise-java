ad14d08ba2ae4e08180a0a3180c6c50678b49cef
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-07 18:02:40 -0500

    Completed test class of hasDoubleK() method

--- codingbat/src/codingBat/hasDoubleKTest.java
+++ codingbat/src/codingBat/hasDoubleKTest.java
@@ -9,17 +9,28 @@ public class hasDoubleKTest {
 
 	@Test
 	public void hasDoubleKHappyCase() {
-		Assertions.assertEquals(false, ArrayUtil.hasDoubleK(new int[] {1, 2, 3, 4}, 1));
-		Assertions.assertEquals(true, ArrayUtil.hasDoubleK(new int[] {1, 2, 3, 4}, 1));
+		Assertions.assertEquals(
+				false, ArrayUtil.hasDoubleK(
+						new int[] {1, 2, 3, 4}, 1));
+		
+		Assertions.assertEquals(
+				true, ArrayUtil.hasDoubleK(
+						new int[] {1, 3, 3, 4, 2, 3, 3, 3}, 3));
 	}
 
 	@Test
 	public void hasDoubleKArrIsNull() {
-		Assertions.assertEquals(false, ArrayUtil.hasDoubleK(new int[] {1, 2, 3, 4}, 1));
+		Assertions.assertEquals(
+				false, 
+				ArrayUtil.hasDoubleK(
+						null, 1));
 	}
 	
 	@Test 
 	public void hasDoubleKArrLengthisLessThan2() {
-		Assertions.assertEquals(false, ArrayUtil.hasDoubleK(new int[] {1, 2, 3, 4}, 1));
+		Assertions.assertEquals(
+				false, 
+				ArrayUtil.hasDoubleK(
+						new int[] {1}, 1));
 	}
 }
