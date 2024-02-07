7b7610b2fb3a67b9793846c5ad47fd4041cd60f4
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-07 17:57:07 -0500

    Updated hasDoubleK() method to search for consecutive k values

--- codingbat/src/codingBat/ArrayUtil.java
+++ codingbat/src/codingBat/ArrayUtil.java
@@ -217,7 +217,7 @@ public class ArrayUtil {
 			return false;
 		}
 		for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] == 2 && arr[i + 1] == 2) {
+			if (arr[i] == k && arr[i + 1] == k) {
 				return true;
 			}
 		}
