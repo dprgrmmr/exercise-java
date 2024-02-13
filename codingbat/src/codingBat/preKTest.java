09d956cf86aec1fe62a6ab9c3d4adf698ceb09a1
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-12 22:41:29 -0500

    Created test class for preK method

--- codingbat/src/codingBat/preKTest.java
+++ codingbat/src/codingBat/preKTest.java
@@ -2,28 +2,44 @@ package codingBat;
 
 import java.util.Arrays;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class preKTest {
 
 	@Test
 	public void preKHappyCase() {
-		Assertions.assertTrue(Arrays.equals(new int[0], new int[0]));
+		Assertions.assertTrue(
+				Arrays.equals(
+						new int[] {1, 6, 7, 8}, 
+						ArrayUtil.preK(
+								new int[] {1, 6, 7, 8, 2, 5}, 2)));
 	}
 	
 	@Test
 	public void preKReturnsInputArrayWhenKIsNotInTheArray() {
-		Assertions.assertTrue(Arrays.equals(new int[0], new int[0]));
+		Assertions.assertTrue(
+				Arrays.equals(
+						new int[] {1, 8, 5, 4, 9}, 
+						ArrayUtil.preK(
+								new int[] {1, 8, 5, 4, 9}, 6)));
 	}
 	
 	@Test
 	public void preKArrayIsNullReturnsNull() {
-		Assertions.assertTrue(Arrays.equals(new int[0], new int[0]));
+		Assertions.assertTrue(
+				Arrays.equals(
+						null, 
+						ArrayUtil.preK(
+								null, 2)));
 	}
 	
 	@Test
 	public void preKEmptyArrayReturnsEmptyArray() {
-		Assertions.assertTrue(Arrays.equals(new int[0], new int[0]));
+		Assertions.assertTrue(
+				Arrays.equals(
+						new int[0], 
+						ArrayUtil.preK(
+								new int[0], 2)));
 	}
 }
