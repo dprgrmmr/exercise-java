a5896d648fbc6ea18c70cb9d2ca94a110aceff8b
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-13 22:35:45 -0500

    Updated countEvens method

--- codingbat/src/codingBat/ArrayUtil.java
+++ codingbat/src/codingBat/ArrayUtil.java
@@ -286,7 +286,13 @@ public class ArrayUtil {
 		return newArr;
 	}
 	
-	public static int[] countEvens(int[] arr) {
-		return arr;
+	public static int countEvens(int[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 == 0) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
