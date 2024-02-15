882e8251cebfce1009f0d5acd7c867e63bc924ff
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-14 22:16:25 -0500

    Check-in of countEvens test class

---
+++ codingbat/src/codingBat/countEvensTest.java
@@ -0,0 +1,39 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class countEvensTest {
+
+	@Test
+	public void countEvensHappyCase() {
+		Assertions.assertEquals(
+				3, 
+				ArrayUtil.countEvens(
+						new int[] { 2, 3, 2, 4, 7, 5 }));
+	}
+
+	@Test
+	public void countEvensNoEvensInArrayReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				ArrayUtil.countEvens(
+						new int[] { 1, 3, 9, 3, 7, 5 }));
+	}
+
+	@Test
+	public void countEvensNullArrayReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				ArrayUtil.countEvens(
+						null));
+	}
+
+	@Test
+	public void countEvensEmptyArrayReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				ArrayUtil.countEvens(
+						new int[0]));
+	}
+}
