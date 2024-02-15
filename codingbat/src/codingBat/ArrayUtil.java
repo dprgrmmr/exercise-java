6cac5252422db790a3d93ba0a261e16b36e048b3
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-14 22:15:17 -0500

    Updated countEvens method to check for special cases

--- codingbat/src/codingBat/ArrayUtil.java
+++ codingbat/src/codingBat/ArrayUtil.java
@@ -287,6 +287,14 @@ public class ArrayUtil {
 	}
 	
 	public static int countEvens(int[] arr) {
+		
+		if (arr == null) {
+			return 0;
+		}
+		
+		if (arr.length < 1) {
+			return 0;
+		}
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] % 2 == 0) {
