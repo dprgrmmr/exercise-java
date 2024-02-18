8b3567cb553ca49567bbcc1051a66e25181a046c
dprgrmr <vinegamerzthechannel@gmail.com>
2024-02-17 21:11:59 -0500

    Updated test class for noK method

--- codingbat/src/codingBat/noKTest.java
+++ codingbat/src/codingBat/noKTest.java
@@ -7,21 +7,37 @@ public class noKTest {
 
 	@Test
 	public void noKKIsNotInArrayReturnsTrue() {
-		Assertions.assertEquals(true, ArrayUtil.noK(null, 0));
+		Assertions.assertEquals(
+				true, 
+				ArrayUtil.noK(
+						new int[] {1, 8, 4, 9, 2, 16}, 
+						3));
 	}
 	
 	@Test
 	public void noKKIsInArrayReturnsFalse() {
-		Assertions.assertEquals(false, ArrayUtil.noK(null, 0));
+		Assertions.assertEquals(
+				false, 
+				ArrayUtil.noK(
+						new int[] {1, 8, 4, 9, 2, 16}, 
+						8));
 	}
 	
 	@Test
 	public void noKNullArrayReturnsTrue() {
-		Assertions.assertEquals(false, ArrayUtil.noK(null, 0));
+		Assertions.assertEquals(
+				true, 
+				ArrayUtil.noK(
+						null, 
+						0));
 	}
 	
 	@Test
 	public void noKEmptyArrayReturnsTrue() {
-		Assertions.assertEquals(false, ArrayUtil.noK(null, 0));
+		Assertions.assertEquals(
+				true, 
+				ArrayUtil.noK(
+						new int[0], 
+						0));
 	}
 }
