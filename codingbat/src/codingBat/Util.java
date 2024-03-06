9b207a5571ae9dc9f52bdb954005819bebaba859
dprgrmr <vinegamerzthechannel@gmail.com>
2024-03-05 22:21:38 -0500

    Initial check-in of changeStr method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -9,4 +9,17 @@ public class Util {
 		}
 		return seq(n-1) + n + "\r\n";
 	}
+	
+	public static String changeStr(String str, String str2) {
+		
+		if (str == null) {
+			return str;
+		}
+		
+		if (str2 == null) {
+			changeStr(str, "null");
+		}
+		
+		return str;
+	}
 }
