64ca49711df03c0ac6ba42de333d6ead86506e54
dprgrmr <vinegamerzthechannel@gmail.com>
2024-03-07 18:47:29 -0500

    Updated changeStr method to fix a bug

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -10,24 +10,29 @@ public class Util {
 		return seq(n-1) + n + "\r\n";
 	}
 	
-	public static String changeStr(String str, String str2) {
+	public static String changeStr(String str, String str2, String str3) {
 		
 		if (str == null) {
 			return str;
 		}
 
 		if (str2 == null) {
-			changeStr(str, "null");
+			return changeStr(str,"null", "null");
+		}
+		
+		if (str3 == null) {
+			return changeStr(str, str2, "null");
 		}
 
 		if (str.length() == 0) {
 			return "";
 		}
 		if (str.substring(0, str2.length()).equals(str2)) {
-			return str2 + changeStr(str.substring(str2.length()), str2);
+			return str3 + changeStr(str.substring(str2.length()), str2, str3);
 		}
-		return str.substring(0, 1) + changeStr(str.substring(1), str2);
+		return str.substring(0, 1) + changeStr(str.substring(1), str2, str3);
 	}
+	
 	
 	public static String seqTwo(int start, int end) {
 		if (end < start) {
