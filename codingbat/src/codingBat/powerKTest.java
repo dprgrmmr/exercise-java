7fe78de211f3bae28059ad5655f5b15e33b654c8
dprgrmr <vinegamerzthechannel@gmail.com>
2024-03-21 22:30:06 -0400

    Initial check-in of powerK test class

---
+++ codingbat/src/codingBat/powerKTest.java
@@ -0,0 +1,34 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class powerKTest {
+
+	@Test
+	public void powerKHappyCase() {
+		Assertions.assertEquals(
+				32, 
+				Util.powerK(
+						2, 
+						5));
+	}
+	
+	@Test
+	public void powerKIsOneReturnsBase() {
+		Assertions.assertEquals(
+				8, 
+				Util.powerK(
+						8, 
+						1));
+	}
+	
+	@Test
+	public void powerKIsZeroReturnsOne() {
+		Assertions.assertEquals(
+				1, 
+				Util.powerK(
+						8, 
+						0));
+	}
+}
