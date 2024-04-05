5ebebbd1679f3ed664c04317e63ea835a5d48d88
dprgrmr <vinegamerzthechannel@gmail.com>
2024-04-05 09:19:26 -0400

    Test class for arrayK method

---
+++ codingbat/src/codingBat/arrayKTest.java
@@ -0,0 +1,37 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class arrayKTest {
+
+	@Test
+	public void arrayKHappyCase() {
+		Assertions.assertEquals(
+				2, 
+				Util.arrayK(
+						new int[] {1, 2, 5, 6, 5, 8}, 
+						5, 
+						1));
+	}
+	
+	@Test
+	public void arrayKNumLengthIsZeroReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				Util.arrayK(
+						new int[0], 
+						5, 
+						1));
+	}
+	
+	@Test
+	public void arrayKIndexOutOfBoundsReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				Util.arrayK(
+						new int[] {1, 2, 5, 6, 5, 8}, 
+						5, 
+						6));
+	}
+}
