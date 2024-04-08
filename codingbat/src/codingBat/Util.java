02210e5d9e88e7c4e04cd182ab05d5895f12c962
dprgrmr <vinegamerzthechannel@gmail.com>
2024-04-07 21:24:22 -0400

    Initial check-in of countPairs method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -288,4 +288,15 @@ public class Util {
 		}
 		return arrayK(nums, k, index + 1);
 	}
+	
+	public int countPairs(String str) {
+		  if (str.length() <=2){
+		    return 0;
+		  }
+		   if (str.substring(0,1).equals(str.substring(2,3))){
+		  return 1 + countPairs(str.substring(1));
+
+		  }
+		  return countPairs(str.substring(1));
+		}
 }
