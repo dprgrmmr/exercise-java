5bfc574083e562d0b3f4a360f5910f0867edac6e
dprgrmr <vinegamerzthechannel@gmail.com>
2024-04-07 21:27:34 -0400

    Formatted countPairs method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -294,14 +294,15 @@ public class Util {
 		if (str == null) {
 			return 0;
 		}
-		
-		  if (str.length() <=2){
-		    return 0;
-		  }
-		   if (str.substring(0,1).equals(str.substring(2,3))){
-		  return 1 + countPairs(str.substring(1));
 
-		  }
-		  return countPairs(str.substring(1));
+		if (str.length() <= 2) {
+			return 0;
 		}
+		
+		if (str.substring(0, 1).equals(str.substring(2, 3))) {
+			return 1 + countPairs(str.substring(1));
+
+		}
+		return countPairs(str.substring(1));
+	}
 }
