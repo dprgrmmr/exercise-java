bca6be01925620a5d4b211ff6c163043ce9ae258
dprgrmr <vinegamerzthechannel@gmail.com>
2024-04-23 22:31:59 -0400

    Initial check-in of changePi method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -400,4 +400,21 @@ public class Util {
 
 		return k * factorialK(k - 1);
 	}
+	
+	public static String changePi(String str) {
+		
+		if (str == null) {
+			return null;
+		}
+
+		if (str.length() < 2) {
+			return str;
+		}
+
+		if (str.substring(0, 2).equals("pi")) {
+			return "3.14" + changePi(str.substring(2));
+		}
+
+		return str.substring(0, 1) + changePi(str.substring(1));
+	}
 }
