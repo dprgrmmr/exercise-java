15d702df609778a66b3cfce6ef1a5dd635afd8a5
dprgrmr <vinegamerzthechannel@gmail.com>
2024-05-02 21:42:59 -0400

    Initial check-in of bunnyEars2 method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -474,4 +474,17 @@ public class Util {
 		
 		return 2 + bunnyEars(bunnies - 1);
 	}
+	
+	public static int bunnyEars2(int bunnies) {
+		
+		if (bunnies == 0) {
+			return 0;
+		}
+		
+		if (bunnies % 2 == 0) {
+			return 3 + bunnyEars2(bunnies - 1);
+		}
+		
+		return 2 + bunnyEars2(bunnies - 1);
+	}
 }
