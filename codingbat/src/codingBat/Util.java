a0d66e0b772a3987e7bebdfe16c15efb8f03fda4
dprgrmr <vinegamerzthechannel@gmail.com>
2024-05-06 22:36:27 -0400

    Initial check-in of allStar method

--- codingbat/src/codingBat/Util.java
+++ codingbat/src/codingBat/Util.java
@@ -514,4 +514,13 @@ public class Util {
 		
 		return array220(nums, index + 1);
 	}
+
+	public static String allStar(String str) {
+		
+		if (str.length() <= 1) {
+			return str;
+		}
+		
+		return str.substring(0, 1) + "*" + allStar(str.substring(1));
+	}
 }
