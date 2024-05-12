b01d25125df825106c0d95e68fd6e0a34fadf4e3
dprgrmr <vinegamerzthechannel@gmail.com>
2024-05-11 22:16:52 -0400

    Initial check-in of test class for array220 method

---
+++ codingbat/src/codingBat/array220Test.java
@@ -0,0 +1,34 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class array220Test {
+
+	@Test
+	public void array220HappyCase() {
+		Assertions.assertEquals(
+				true, 
+				Util.array220(
+						new int[] {1, 4, 3, 30, 8}, 
+						0));
+	}
+	
+	@Test
+	public void array220EmptyArrayReturnsFalse() {
+		Assertions.assertEquals(
+				false, 
+				Util.array220(
+						new int[0], 
+						0));
+	}
+	
+	@Test
+	public void array220IndexTooBigReturnsFalse() {
+		Assertions.assertEquals(
+				false, 
+				Util.array220(
+						new int[] {7, 3, 8, 8},
+						4));
+	}
+}
