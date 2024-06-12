0df706b0b6efd0ac697ccad4e1bf4228580dd531
dprgrmr <vinegamerzthechannel@gmail.com>
2024-06-11 21:47:00 -0400

    Initial check-in of test class for array6 method

---
+++ codingbat/src/codingBat/array6Test.java
@@ -0,0 +1,43 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class array6Test {
+
+	@Test
+	public void array6HappyCase() {
+		Assertions.assertEquals(
+				true, 
+				Util.array6(
+						new int[] {1, 6, 6, 2, 6}, 
+						1));
+	}
+	
+	@Test
+	public void array6NoSixFoundReturnsFalse() {
+		Assertions.assertEquals(
+				false, 
+				Util.array6(
+						new int[] {1, 6, 6, 2, 7}, 
+						3));
+	}
+	
+	@Test
+	public void array6EmptyArrayReturnsFalse() {
+		Assertions.assertEquals(
+				false, 
+				Util.array6(
+						new int[0], 
+						3));
+	}
+	
+	@Test
+	public void array6IndexGreaterThanArrayLengthReturnsFalse() {
+		Assertions.assertEquals(
+				false, 
+				Util.array6(
+						new int[] {1, 6, 6, 2, 7}, 
+						5));
+	}
+}
