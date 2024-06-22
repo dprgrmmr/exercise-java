2367ed11e530ffb1f73d566335aa934c321d3196
dprgrmr <vinegamerzthechannel@gmail.com>
2024-06-21 21:47:48 -0400

    Initial check-in of test class for sumOddDigits method

---
+++ codingbat/src/codingBat/sumOddDigitsTest.java
@@ -0,0 +1,31 @@
+package codingBat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class sumOddDigitsTest {
+	
+	@Test
+	public void sumOddDigitsHappyCase() {
+		Assertions.assertEquals(
+				22, 
+				Util.sumOddDigits(
+						3745272));
+	}
+	
+	@Test
+	public void sumOddDigitsNoOddDigitsReturnsZero() {
+		Assertions.assertEquals(
+				0, 
+				Util.sumOddDigits(
+						28640));
+	}
+	
+	@Test
+	public void sumOddDigitsInputIsOneDigitAndOddReturnsInput() {
+		Assertions.assertEquals(
+				7, 
+				Util.sumOddDigits(
+						7));
+	}
+}
