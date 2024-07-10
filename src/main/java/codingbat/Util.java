1d38beb8b6b6c9f3054c2762f04e6681b73ffc69
dprgrmr <vinegamerzthechannel@gmail.com>
2024-07-09 20:58:00 -0400

    Initial check-in of count11 method

--- src/main/java/codingbat/Util.java
+++ src/main/java/codingbat/Util.java
@@ -992,4 +992,19 @@ public class Util {
 			return countHi(str.substring(0, str.length() - 1));
 		}
 
+	public int count11(String str) {
+
+			if (str == null){
+				return 0;
+			}
+			
+		if (str.length() < 2){
+			return 0;
+		}
+
+		if (str.substring(0, 2).equals("11")){
+			return 1 + count11(str.substring(2));
+		}
+		return count11(str.substring(1));
+	}
 }
