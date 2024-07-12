b5b1983e1532a4f88ff2fbe947fad5f83971e815
dprgrmr <vinegamerzthechannel@gmail.com>
2024-07-11 21:05:50 -0400

    Initial check-in of countHi2 method

--- src/main/java/codingbat/Util.java
+++ src/main/java/codingbat/Util.java
@@ -1015,4 +1015,25 @@ public class Util {
 		}
 		return count11(str.substring(1));
 	}
+
+	public static int countHi2(String str) {
+
+		if (str == null){
+			return 0;
+		}
+
+		if (str.length() < 1){
+			return 0;
+		}
+
+		if (str.length() >= 3 && str.substring(0, 3).equals("xhi")){
+			return countHi2(str.substring(3));
+		}
+
+		if (str.length() >=2 && str.substring(0,2).equals("hi")){
+			return 1 + countHi2(str.substring(2));
+		}
+
+		return countHi2(str.substring(1));
+	}
 }
